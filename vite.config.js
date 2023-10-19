acf8ef784b81dc149200f8ad116cfdd34d0b08da
Jimmy <jimmyofchen@hotmail.com>
2023-10-19 13:27:49 -0400

    deployment fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/pokemon-mem-game/",
   plugins: [react()],
-  base: '/pokemon-mem-game/'
 })
